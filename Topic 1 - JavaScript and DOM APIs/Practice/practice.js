63c781993e51d049db84ab1459e1dde96a6c3a94
BrunoMPedraza <61323575+BrunoMPedraza@users.noreply.github.com>
2021-01-29 15:14:41 -0300

    Topic 1 - Excercise 1
    Create a file called index.html with the correct doctype and some random content.
    
    Add a stylesheet to the HTML file and use it to center the texts of all section elements.
    
    Add a hidden section with Hello World inside of it.
    
    When the page finished loading the section must fade in

--- Topic 1 - JavaScript and DOM APIs/Practice/practice.js
+++ Topic 1 - JavaScript and DOM APIs/Practice/practice.js
@@ -4,8 +4,3 @@ const fade = () => setTimeout(() => (section.classList.toggle("fadeout")),500);
 
 
 section.addEventListener("load", fade());
-
-function scriptAlert() {
-    document.getElementById("alertscript");
-    window.alert("Secret");
-}   
